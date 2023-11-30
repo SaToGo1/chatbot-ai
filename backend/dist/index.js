c7b6e3ec10020c6a52cf5022e9eaea7e746b3db5
SaToGo1 <stoledano97@gmail.com>
2023-11-30 12:11:58 +0100

    First Route

---
+++ backend/dist/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+app.get("/hello", (req, res, next) => {
+    return res.send("Hello");
+});
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server Open at port ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,11 @@
-console.log(
-  ""
-);
+import express from 'express';
+
+const app = express();
+
+app.get("/hello", (req, res, next) => {
+  return res.send("Hello")
+})
+const PORT = 5000
+app.listen(PORT, () => {
+  console.log(`Server Open at port ${PORT}`)
+})
