36db43e6799dff079c8d0aaad24c8e25aab83e9e
SaToGo1 <stoledano97@gmail.com>
2023-11-30 13:01:01 +0100

    Backend structure

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import { config } from "dotenv";
+config();
 const app = express();
+app.use(express.json());
 app.get("/hello", (req, res, next) => {
     return res.send("Hello");
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
-
+import { config } from "dotenv";
+config();
 const app = express();
+
+app.use(express.json())
 
 app.get("/hello", (req, res, next) => {
   return res.send("Hello")
 })
+
 const PORT = 5000
 app.listen(PORT, () => {
   console.log(`Server Open at port ${PORT}`)
