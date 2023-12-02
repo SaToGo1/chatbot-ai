25999575dd751ca767eb155c377d11fd299ba268
SaToGo1 <stoledano97@gmail.com>
2023-12-02 23:23:45 +0100

    Add user signup route

--- backend/dist/controllers/user-controller.js
+++ backend/dist/controllers/user-controller.js
@@ -1,9 +1,26 @@
 import User from "../models/User.js";
+import bcryptjs from 'bcryptjs';
+const { hash } = bcryptjs;
 export const getAllUsers = async (req, res, next) => {
     try {
         // get all users for database
         const users = await User.find({});
         return res.status(200).json({ message: "OK", users });
+    }
+    catch (error) {
+        console.log(error);
+        const message = error.message;
+        return res.status(200).json({ message: "ERROR", cause: message });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    try {
+        // user signup
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        return res.status(200).json({ message: "OK", id: user._id.toString() });
     }
     catch (error) {
         console.log(error);
