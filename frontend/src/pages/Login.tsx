f4ca90711b0df5c9e61bd1c8f740f5dcfd6dc4fd
SaToGo1 <stoledano97@gmail.com>
2024-01-09 11:20:30 +0100

    navigate to /Chat after successful Login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,31 +1,35 @@
 import { Typography } from '@mui/material';
 import HeaderLogin from '../components/header-login';
-import { CSSProperties, ReactNode } from 'react';
+import { CSSProperties } from 'react';
 import { IoLogInOutline } from 'react-icons/io5';
 
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 // import { FormControl, FormLabel } from '@mui/material'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast'
 import { useAuth } from '../context/auth-context';
 
 export default function Login() {
   const auth = useAuth()
+  const navigate = useNavigate();
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const email = formData.get("Email") as string;
     const password = formData.get("Password") as string;
     try {
-      toast.loading('signing In', { id: 'login' })
-      await auth?.login(email, password)
-      toast.success('signed In Successfully', { id: 'login' })
+      toast.loading('signing In', { id: 'login' });
+      await auth?.login(email, password);
+      toast.success('signed In Successfully', { id: 'login' });
+      navigate('/chat')
     } catch(error) {
-      console.log(error)
-      toast.error('signing In Failed', { id: 'login' })
+      console.log(error);
+      toast.error('signing In Failed', { id: 'login' });
     }
   };
+
   return (
     <>
       <HeaderLogin />
